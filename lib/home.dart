ae2d8152a0fcec0cbc721045b07dbe92151053e3
ryka-1104 <gyd90621@gmail.com>
2026-03-01 17:39:05 +0900

    カメラでQRコード読み取る

---
+++ lib/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class MyHomeApp extends StatelessWidget {
+  const MyHomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: QRScanOnlyPage(
+        onDetected: (value) {
+          debugPrint('QRコードの値: $value');
+        },
+      ),
+    );
+  }
+}
+
+class QRScanOnlyPage extends StatefulWidget {
+  const QRScanOnlyPage({super.key, required this.onDetected});
+
+  final void Function(String value) onDetected;
+
+  @override
+  State<QRScanOnlyPage> createState() => _QRScanOnlyPageState();
+}
+
+class _QRScanOnlyPageState extends State<QRScanOnlyPage> {
+  bool _detected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MobileScanner(
+        onDetect: (capture) {
+          if (_detected) return;
+
+          final barcode = capture.barcodes.first;
+          final value = barcode.rawValue;
+
+          if (value != null) {
+            _detected = true;
+            widget.onDetected(value);
+          }
+        },
+      ),
+    );
+  }
+}
