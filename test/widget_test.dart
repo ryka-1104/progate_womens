06f4ca7577951f261756d98789d107b63f0118d6
ryka-1104 <gyd90621@gmail.com>
2026-03-04 21:31:45 +0900

    testの修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:progate_womens/main.dart';
+import 'package:progate_womens/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyHomeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
