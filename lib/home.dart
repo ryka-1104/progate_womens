72fd514a6798bdfee0e6a256dba500d7b5755c50
ryka-1104 <gyd90621@gmail.com>
2026-03-05 17:26:13 +0900

    デフォルトのホーム画面の作成、カメラ機能用のファイルの作成

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 
 class MyHomeApp extends StatelessWidget {
   const MyHomeApp({super.key});
@@ -8,43 +7,25 @@ class MyHomeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: QRScanOnlyPage(
-        onDetected: (value) {
-          debugPrint('QRコードの値: $value');
-        },
-      ),
+      home: MyHomePage(onDetected: (value) {}),
     );
   }
 }
 
-class QRScanOnlyPage extends StatefulWidget {
-  const QRScanOnlyPage({super.key, required this.onDetected});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.onDetected});
 
   final void Function(String value) onDetected;
 
   @override
-  State<QRScanOnlyPage> createState() => _QRScanOnlyPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _QRScanOnlyPageState extends State<QRScanOnlyPage> {
-  bool _detected = false;
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MobileScanner(
-        onDetect: (capture) {
-          if (_detected) return;
-
-          final barcode = capture.barcodes.first;
-          final value = barcode.rawValue;
-
-          if (value != null) {
-            _detected = true;
-            widget.onDetected(value);
-          }
-        },
-      ),
+      // ここにホーム画面のUIを実装する
     );
   }
 }
